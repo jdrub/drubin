3148dfb5851eb788d717c77b178e8f25d344b0b4
Joshua Drubin <j.drubin94@gmail.com>
2018-11-12 01:04:28 -0500

    add colors, overlay, order tiles

--- src/Home.js
+++ src/Home.js
@@ -5,24 +5,27 @@ import MainPhoto from './mainPhoto.jpg';
 export default () => {
     return (
         <AppContainer>
-            <Row>
-                <Tile />
-                <Tile />
-                <Tile />
-            </Row>
-            <Row>
-                <Tile />
-                <Tile imgSrc={MainPhoto} />
-                <Tile />
-            </Row>
-            <Row>
-                <Tile />
-                <Tile />
-                <Tile />
-            </Row>
+            <div>
+                <Row>
+                    <Tile color="#FCD4E4" />
+                    <Tile color="#CFF6F4" />
+                    <Tile color="#FFFCD7" />
+                </Row>
+                <Row>
+                    <Tile color="#F4ADCA" />
+                    <PhotoTile color="#9FE0DD" imgSrc={MainPhoto} />
+                    <Tile color="#FFFAB5" />
+                </Row>
+                <Row>
+                    <Tile color="#E593B4" />
+                    <Tile color="#75B6B4" />
+                    <Tile color="#FFF9A4" />
+                </Row>
+            </div>
         </AppContainer>
     );
 }
+
 
 const AppContainer = styled.div`
     display: flex;
@@ -36,6 +39,8 @@ const AppContainer = styled.div`
 `;
 
 const Tile = styled.div`
+    display: inline-block;
+
     height: 150px;
     width: 150px;
 
@@ -46,6 +51,7 @@ const Tile = styled.div`
     border-color: black;
     border-style: solid;
 
+    background-color: ${p => p.color};
     ${p => p.imgSrc
         ? css`
             background-image: url(${p.imgSrc});
@@ -53,6 +59,25 @@ const Tile = styled.div`
             background-position: 49% 31%;
             `
         : ''
+    }
+`;
+
+const PhotoTile = styled(Tile)`
+    position: relative;
+
+    &:before {
+        position: absolute;
+        display: block;
+
+        top:0;
+        left:0;
+        width:100%;
+        height:100%;
+
+        content:" ";
+
+        background-color: ${p => p.color};
+        opacity: 0.4;
     }
 `;
 
